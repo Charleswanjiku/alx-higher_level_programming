a702c938397914ba787b1a366f79ec44d8ae78c6
Charleswanjiku <kangethewanjiku@gmail.com>
2022-09-22 10:50:32 +0000

    import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
-    import sys
-
-    value = 0
-    for i in range(len(sys.argv) - 1):
-        value = value + int(sys.argv[i + 1])
-        print("{}".format(value))
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))

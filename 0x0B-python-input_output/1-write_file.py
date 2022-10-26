98c0ba1f0e8d6a9bf6e58fb0d6b15b702cf3ba65
Charleswanjiku <kangethewanjiku@gmail.com>
2022-10-26 07:03:31 +0000

    input output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename, mode="w", encoding="utf-8") as fd:
-        fd.write(text)
-    return len(text)
+    with open(filename, "w", encoding="UTF-8") as f:
+        return f.write(text)
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def append_write(filename="", text=""):
-    with open(filename, mode="a", encoding="utf-8") as fd:
-        fd.write(text)
-    return len(text)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
